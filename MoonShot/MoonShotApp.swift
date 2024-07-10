9174402daf251a6bffecbe1db1eb626e5b5ac924
harsh <harsh.pandey77@gmail.com>
2024-07-10 09:26:10 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by harsh  on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
